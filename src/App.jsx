e54bc72a7571a8f2e5661aa90e7745c139961021
Ekele1 <jerryoilfarm01@gmail.com>
2024-02-29 03:10:23 +0100

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,70 @@
 import React from "react";
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import './App.css'
 import HomePage from "./LandingPage";
 import VerifyOtp from "./Components/onboarding/OtpVerification";
 import MainPage from "./Components/mainPage/MainPage";
-import Settings from "./Components/settings/AllSettings";
+// import Settings from "./Components/settings/AllSettings";
 import PasswordForget from "./Components/onboarding/ForgotYourPassword";
 import Login from "./Components/onboarding/Loginyou";
 import SignUp from "./Components/onboarding/Signup";
 import ResetPassword from "./Components/onboarding/ResetYourPassword";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage/>
-  },
-  {
-    path: 'login',
-    element: <Login />
-  },
-  {
-    path: "forget",
-    element : <PasswordForget />
-  },
-  {
-    path: '/signup',
-    element: <SignUp />
-  },
-  {
-    path: "verify",
-    element: <VerifyOtp />
-  },
-  {
-    path: "reset",
-    element: <ResetPassword />
-  },
-  {
-    path: "main",
-    element: <MainPage />
-  },
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <HomePage/>
+//   },
+//   {
+//     path: 'login',
+//     element: <Login />
+//   },
+//   {
+//     path: "forget",
+//     element : <PasswordForget />
+//   },
+//   {
+//     path: '/signup',
+//     element: <SignUp />
+//   },
+//   {
+//     path: "verify",
+//     element: <VerifyOtp />
+//   },
+//   {
+//     path: "reset",
+//     element: <ResetPassword />
+//   },
+//   {
+//     path: "main",
+//     element: <MainPage />
+//   },
 
-  {
-    path: "set",
-    element: <Settings />
-  },
-])
+//   {
+//     path: "set",
+//     element: <Settings />
+//   },
+// ])
 
 function App() {
 
   return (
     <>
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      <HashRouter >
+      <Routes>
+        <Route>
+          <Route path='/' element={<HomePage />}/>
+          <Route path='login' element={<Login />}/>
+          <Route path='forget' element={<PasswordForget />}/>
+          <Route path='signup' element={<SignUp />}/>
+          <Route path='verify' element={<VerifyOtp />}/>
+          <Route path='reset' element={<ResetPassword />}/>
+          <Route path='main' element={<MainPage />}/>
+        </Route>
+      </Routes>
+    </HashRouter>
     </>
   )
 }
